9de35a3253300faff76a06f147aad6906181d5d7
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2023-08-30 08:48:37 -0700

    crm, prop radar, and webhook modules added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CrmModule } from './crm/crm.module';
+import { PropertyRadarModule } from './property-radar/property-radar.module';
+import { WebhookModule } from './webhook/webhook.module';
 
 @Module({
-  imports: [],
+  imports: [CrmModule, PropertyRadarModule, WebhookModule],
   controllers: [AppController],
   providers: [AppService],
 })
