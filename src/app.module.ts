551d3037bc5a4de695a0e5f38ed88e1318ae2028
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2023-09-18 13:20:57 -0700

    OAuth flow implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,14 @@ import { AppService } from './app.service';
 import { CrmModule } from './crm/crm.module';
 import { PropertyRadarModule } from './property-radar/property-radar.module';
 import { WebhookModule } from './webhook/webhook.module';
-// import { OAuthController } from './oauth/oauth.controller';
+import { OAuthController } from './oauth/oauth.controller';
 import { WorkflowManagerService } from './workflow-manager/workflow-manager.service';
 import { SupabaseService } from './supabase/supabase.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [CrmModule, PropertyRadarModule, WebhookModule],
-  controllers: [AppController],
+  imports: [CrmModule, PropertyRadarModule, WebhookModule, HttpModule],
+  controllers: [AppController, OAuthController],
   providers: [AppService, WorkflowManagerService, SupabaseService],
 })
 export class AppModule {}
--- src/oauth/oauth.controller.ts
+++ src/oauth/oauth.controller.ts
@@ -1,19 +1,70 @@
-// import { Controller, Get, Query, Res } from '@nestjs/common';
-// import { Response } from 'express';
+import { Controller, Get, Query, Res } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { Response } from 'express';
+import { SupabaseService } from 'src/supabase/supabase.service';
+import { map } from 'rxjs/operators';
+import { firstValueFrom } from 'rxjs';
 
-// @Controller('oauth')
-// export class OAuthController {
-//   @Get('callback')
-//   async handleOAuthCallback(@Query('code') code: string, @Res() res: Response) {
-//     // Here, 'code' is the authorization code you get from Go High Level
-//     // Use this code to get an access token
-//     // Your OAuth 2.0 logic here
-//     const locationId = await this.getLocationId(code);
-//     // Assuming you successfully obtain an access token and validate it...
+@Controller('oauth')
+export class OAuthController {
+  constructor(
+    private readonly supabaseService: SupabaseService,
+    private readonly httpService: HttpService,
+  ) {}
 
-//     // Redirect to the main CRM page
-//     res.redirect(
-//       `https://crm.proinsight.us/v2/location/${locationId}/dashboard`,
-//     );
-//   }
-// }
+  @Get('callback')
+  async handleOAuthCallback(@Query('code') code: string, @Res() res: Response) {
+    // Exchange code for access token
+    const accessTokenData = await this.getAccessToken(code);
+
+    // Extract the access token, refresh token, and location ID
+    const {
+      access_token: accessToken,
+      refresh_token: refreshToken,
+      location_id: locationId,
+    } = accessTokenData;
+
+    await this.supabaseService.addRecord('oauth_tokens', {
+      location_id: locationId,
+      access_token: accessToken,
+      refresh_token: refreshToken,
+    });
+
+    // Redirect the user to their dashboard on Go High Level
+    const redirectUrl = `https://crm.proinsight.us/v2/location/${locationId}/dashboard`;
+    res.redirect(redirectUrl);
+  }
+
+  private async getAccessToken(code: string): Promise<any> {
+    try {
+      const observable = this.httpService.post(
+        'https://api.gohighlevel.com/OAuth/token',
+        {
+          client_id: process.env.CLIENT_ID,
+          client_secret: process.env.CLIENT_SECRET,
+          code,
+          redirect_uri:
+            'https://pro-insight-isb4sdtn5-andrewjamesboyle.vercel.app/oauth/callback',
+          grant_type: 'authorization_code',
+        },
+      );
+
+      const response = await firstValueFrom(
+        observable.pipe(map((axiosResponse) => axiosResponse.data)),
+      );
+
+      if (response && response.access_token) {
+        return {
+          access_token: response.access_token,
+          refresh_token: response.refresh_token,
+          locationId: response.locationId,
+        };
+      } else {
+        throw new Error('No access token received');
+      }
+    } catch (error) {
+      console.error('Error obtaining access token:', error);
+      throw error;
+    }
+  }
+}
